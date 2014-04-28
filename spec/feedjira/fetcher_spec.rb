9efd6a03c859858882449d5e2b973ad57124024a
Jon Allured <jon.allured@gmail.com>
2014-04-28 18:36:29 -0500

    Test the parser and fetcher directly
    Let the integration tests cover the factory methods, stick to instance
    methods in these tests.

--- spec/feedjira/fetcher_spec.rb
+++ spec/feedjira/fetcher_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe Feedjira::Fetcher do
-  it 'fetches' do
-    xml = Feedjira::Fetcher.fetch 'http://feedjira.com/blog/feed.xml'
-    expect(xml).to eq sample_atom_middleman_feed
+  describe '#fetch' do
+    it 'returns the fetched xml' do
+      fetcher = Feedjira::Fetcher.new 'http://feedjira.com/blog/feed.xml'
+      expect(fetcher.fetch).to eq sample_atom_middleman_feed
+    end
   end
 end
--- spec/feedjira/parser_spec.rb
+++ spec/feedjira/parser_spec.rb
@@ -1,18 +1,31 @@
 require 'spec_helper'
 
 describe Feedjira::Parser do
-  let(:xml) { sample_atom_feed }
+  describe '#parse' do
+    subject(:parser) { Feedjira::Parser.new xml }
 
-  it 'parses' do
-    feed = Feedjira::Parser.parse xml
-    expect(feed).to be_a Feedjira::Parser::Atom
-  end
+    context 'with Atom xml' do
+      let(:xml) { sample_atom_feed }
 
-  context "with xml that can't be parsed" do
-    it 'raises an error' do
-      expect do
-        Feedjira::Parser.parse '<xml></xml>'
-      end.to raise_error Feedjira::Parser::ParserNotFoundError
+      it 'returns an Atom parsed document' do
+        expect(parser.parse).to be_a Feedjira::Parser::Atom
+      end
+    end
+
+    context 'with RSS xml' do
+      let(:xml) { sample_rss_feed }
+
+      it 'returns an RSS parsed document' do
+        expect(parser.parse).to be_a Feedjira::Parser::Rss
+      end
+    end
+
+    context "with xml that can't be parsed" do
+      let(:xml) { '<xml></xml>' }
+
+      it 'raises an error' do
+        expect { parser.parse }.to raise_error Feedjira::Parser::ParserNotFoundError
+      end
     end
   end
 end
