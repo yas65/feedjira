28eb3f2bbda379133db6768799c64e9cdac80e2b
Joshua Davey and Jon Allured <dev+jgdavey+jonallured@hashrocket.com>
2014-04-29 16:30:13 -0500

    Updating feeds no longer supported

--- lib/feedjira/feed_utilities.rb
+++ lib/feedjira/feed_utilities.rb
@@ -36,29 +36,6 @@ module Feedjira
       end
     end
 
-    def updated?
-      @updated || false
-    end
-
-    def new_entries
-      @new_entries ||= []
-    end
-
-    def has_new_entries?
-      new_entries.size > 0
-    end
-
-    def update_from_feed(feed)
-      self.new_entries += find_new_entries_for(feed)
-      self.entries.unshift(*self.new_entries)
-
-      @updated = false
-
-      UPDATABLE_ATTRIBUTES.each do |name|
-        @updated ||= update_attribute(feed, name)
-      end
-    end
-
     def update_attribute(feed, name)
       old_value, new_value = send(name), feed.send(name)
 
@@ -68,32 +45,6 @@ module Feedjira
       else
         false
       end
-    end
-
-    private
-
-    def find_new_entries_for(feed)
-      # this implementation is a hack, which is why it's so ugly.
-      # it's to get around the fact that not all feeds have a published date.
-      # however, they're always ordered with the newest one first.
-      # So we go through the entries just parsed and insert each one as a new entry
-      # until we get to one that has the same id as the the newest for the feed
-      return feed.entries if self.entries.length == 0
-      latest_entry = self.entries.first
-      found_new_entries = []
-      feed.entries.each do |entry|
-        if entry.entry_id.nil? && latest_entry.entry_id.nil?
-          break if entry.url == latest_entry.url
-        else
-          break if entry.entry_id == latest_entry.entry_id || entry.url == latest_entry.url
-        end
-        found_new_entries << entry
-      end
-      found_new_entries
-    end
-
-    def existing_entry?(test_entry)
-      entries.any? { |entry| entry.id == test_entry.id }
     end
   end
 end
